1ef006af467c06cb7a6af31498186cdf86a96ab3
tttol <tkhstol.929@gmail.com>
2026-08-21 06:57:44 +0900

    update

--- src/presentation/render.rs
+++ src/presentation/render.rs
@@ -1,4 +1,4 @@
-use ratatui::{Frame, layout::{Alignment, Constraint, Layout}, widgets::{Block, Paragraph, Row, Table}};
+use ratatui::{Frame, layout::{Alignment, Constraint, Layout}, widgets::{Block, Borders, Paragraph, Row, Table}};
 
 // TITLE
 // BEFORE, AFTER, COMMENT, REVIEWED_AT
@@ -16,30 +16,35 @@ pub fn render(frame: &mut Frame) {
     ])
     .areas(review_area);
 
+    let title = Block::default()
+        .title("TITLE")
+        .borders(Borders::ALL);
+
+    frame.render_widget(title, title_area);
 
 
     ////////////////////////////////////////
-    let [header_area, table_area, footer_area] = Layout::vertical([
-        Constraint::Length(3),
-        Constraint::Min(1),
-        Constraint::Length(3),
-    ])
-    .areas(frame.area());
-
-    let header = Paragraph::new("English Log Viewer")
-        .alignment(Alignment::Center)
-        .block(Block::bordered());
-    let table = Table::new(
-        Vec::<Row>::new(),
-        [Constraint::Length(25), Constraint::Min(20)],
-    )
-    .header(Row::new(["Reviewed at", "Feedback"]))
-    .block(Block::bordered().title(" Reviews "));
-    let footer = Paragraph::new("q Quit")
-        .alignment(Alignment::Center)
-        .block(Block::bordered());
-
-    frame.render_widget(header, header_area);
-    frame.render_widget(table, table_area);
-    frame.render_widget(footer, footer_area);
+    // let [header_area, table_area, footer_area] = Layout::vertical([
+    //     Constraint::Length(3),
+    //     Constraint::Min(1),
+    //     Constraint::Length(3),
+    // ])
+    // .areas(frame.area());
+    //
+    // let header = Paragraph::new("English Log Viewer")
+    //     .alignment(Alignment::Center)
+    //     .block(Block::bordered());
+    // let table = Table::new(
+    //     Vec::<Row>::new(),
+    //     [Constraint::Length(25), Constraint::Min(20)],
+    // )
+    // .header(Row::new(["Reviewed at", "Feedback"]))
+    // .block(Block::bordered().title(" Reviews "));
+    // let footer = Paragraph::new("q Quit")
+    //     .alignment(Alignment::Center)
+    //     .block(Block::bordered());
+    //
+    // frame.render_widget(header, header_area);
+    // frame.render_widget(table, table_area);
+    // frame.render_widget(footer, footer_area);
 }
